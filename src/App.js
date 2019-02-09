976a4d3f6cfe36a2260dfb2819f638fd3ed23af2
Hyejin Shin <enthusiastic.shj@gmail.com>
2019-02-10 01:12:29 +1100

    implement by using react-hooks

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import React, {Component} from 'react';
+import React, {Component, useState} from 'react';
 import './App.css';
 
-class App extends Component {
-  state = {
-    count: 0,
-  };
-  modify = n => {
-    this.setState ({
-      count: n,
-    });
-  };
-  render () {
-    const {count} = this.state;
-
-    return (
-      <div>
-        <div>{count}</div>
-        <button onClick={() => this.modify (count + 1)}>Increment</button>
-      </div>
-    );
-  }
-}
+const App = () => {
+  const [count, setCount] = useState (0); //useState() returns an array
+  // useState which operates as an array gives two elements
+  // first element is value, second element is setCount(it means how to change the value)
+  // useState HOOKS under the management of state of react
+  return (
+    <div>
+      <div>{count}</div>
+      <button onClick={() => setCount (count + 1)}>Increment</button>
+      <button onClick={() => setCount (count - 1)}>Decrement</button>
+    </div>
+  );
+};
 
 export default App;
